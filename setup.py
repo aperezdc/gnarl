148cbd3ae10bb25917bf0737d8a569b46e205cdf
Adrian Perez de Castro <aperez@igalia.com>
2016-03-14 16:46:15 +0200

    Release version 0.0.5

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def file_contents(*relpath):
 if __name__ == "__main__":
     setup(
         name="lasso",
-        version="0.0.4",
+        version="0.0.5",
         description=("Lightweight module to define serializable, "
                      "schema-validated classes"),
         long_description=file_contents("README.rst"),
